69f17f465def0238f4cf7a32d8bf51b537bb5bf2
JasonHanden <jasonhanden17@gmail.com>
2021-02-21 14:16:30 -0800

    Signed-off-by: JasonHanden <jasonhanden17@gmail.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Jason Handen
+ * Created on February 21, 2021, 2:04 PM
+ * Purpose: print HelloWorld 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "Hello World!";
+    return 0;
+}
